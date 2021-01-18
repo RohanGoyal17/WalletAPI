22c7e25be5ab4670ea4cb448128d18845ae360c4
Rohan <rohan17091999@gmail.com>
2021-01-18 22:26:29 +0530

    validations complete

--- src/main/java/com/rest/user/repository/UsersRepository.java
+++ src/main/java/com/rest/user/repository/UsersRepository.java
@@ -3,6 +3,10 @@ package com.rest.user.repository;
 import com.rest.user.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsersRepository extends JpaRepository<Users, String> {       //defining database (object type, id type)
+import java.util.List;
 
+public interface UsersRepository extends JpaRepository<Users, String> {       //defining database (object type, id type)
+    public List<Users> findByEmail(String email); //find by email
+    //public List<Users> findById(String id);
+    public List<Users> findByPhone(Integer phone); //find by phone number
 }
--- src/main/java/com/rest/user/resource/UsersResource.java
+++ src/main/java/com/rest/user/resource/UsersResource.java
@@ -13,16 +13,25 @@ public class UsersResource {
     @Autowired
     UsersRepository usersRepository;        // defining reference
 
+    @GetMapping(value = "/all")
+    public List<Users> displayAll(){
+        return usersRepository.findAll();
+    }
+
     @GetMapping(value = "/user")            //get mapping
     public Users getAll(@RequestParam(value = "userId", defaultValue = "") String id) {
-        //return usersRepository.findAll();
+        //return usersRepository.findAll()
         return usersRepository.findById(id).get();
     }
 
     @PostMapping(value = "/user")           // post mapping
-    public List<Users> persist(@RequestBody final Users users) {
-        usersRepository.save(users);
-        return usersRepository.findAll();
+    public void persist(@RequestBody final Users users) {
+        //if(usersRepository.findById(users.getId()) == null)
+        List<Users> email_id = usersRepository.findByEmail(users.getEmail());  //check for same email
+        List<Users> phone_number = usersRepository.findByPhone(users.getPhone()); // check for same phone number
+        if(email_id.isEmpty() && phone_number.isEmpty()) {
+            usersRepository.save(users);
+        }
     }
 
     @PutMapping(value = "/user")            // put mapping
