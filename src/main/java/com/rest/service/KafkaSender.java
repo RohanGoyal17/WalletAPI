da038f14d2d28ab31eb7297697d7e9edf4a1d38f
Rohan <rohan17091999@gmail.com>
2021-01-24 16:07:55 +0530

    kafka configured to listen on /elastic/transaction

---
+++ src/main/java/com/rest/service/KafkaSender.java
@@ -0,0 +1,19 @@
+package com.rest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaSender {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    String kafkaTopic = "transaction_by_user_id";
+
+    public void send(String message) {
+
+        kafkaTemplate.send(kafkaTopic, message);
+    }
+}
