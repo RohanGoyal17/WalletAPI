8cc25ce9c59b7b361e9f20742e78059e3e900d68
Rohan <rohan17091999@gmail.com>
2021-01-25 01:45:21 +0530

    elastic search demo repo added

---
+++ src/main/java/com/rest/transaction/repository/ElasticRepository.java
@@ -0,0 +1,17 @@
+package com.rest.transaction.repository;
+public interface ElasticRepository{
+
+        }
+
+//import com.rest.transaction.model.Transaction;
+//import java.util.List;
+
+//import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+//public interface ElasticRepository extends ElasticsearchRepository<Transaction, Integer> {       //defining database (object type, id type)
+    //public List<Transaction> findByTransactionid(Integer transactionid); //find by email
+    //public List<Transaction> findBySenderphone(Integer senderphone);
+    //public List<Transaction> findByReceiverphone(Integer receiverphone);
+    //public List<Users> findById(String id);
+    //public List<Wallet> findByPhone(Integer phone); //find by phone number
+//}
